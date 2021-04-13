350125b2d940078cb9612838fc0b1821cbd56432
eh-peter <pbarschall@equalizehealth.org>
2021-04-13 16:24:42 -0700

    Create Simulate button
    Simulate brings up screen.  Button areas on screen are active (1,2,3,4).
    Need commands "button(x)" and "getscreen" active on board.

---
+++ Simulation.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace FlowWorks
+{
+    public partial class Simulation : Form
+    {
+        private bool button1 = false;
+        private bool button2 = false;
+        private bool button3 = false;
+        private bool button4 = false;
+        private Form1 form1;
+
+        public Simulation()
+        {
+            InitializeComponent();
+            Thread stateChangeThread = new Thread(new ThreadStart(this.stateMachine));
+            stateChangeThread.IsBackground = true;
+            stateChangeThread.Start();
+        }
+
+        public Simulation(Form1 form1)
+        {
+            InitializeComponent();
+            this.form1 = form1;
+            Thread stateChangeThread = new Thread(new ThreadStart(this.stateMachine));
+            stateChangeThread.IsBackground = true;
+            stateChangeThread.Start();
+        }
+
+        // This function sends commands to the target to retrieve data information
+        // It runs in a thread, constantly sending the command
+        void stateMachine()
+        {
+            while (true)
+            {
+                if (this.form1.isConnected)
+                {
+                    // This means we request data every 500 msecs (default)
+                    Thread.Sleep(500);
+                    if (button1)
+                    {
+                        this.form1.SendTerminalCommand("button(1)");
+                        button1 = false;
+                    }
+                    else if (button2)
+                    {
+                        this.form1.SendTerminalCommand("button(2)");
+                        button2 = false;
+                    }
+                    else if (button3)
+                    {
+                        this.form1.SendTerminalCommand("button(3)");
+                        button3 = false;
+                    }
+                    else if (button4)
+                    {
+                        this.form1.SendTerminalCommand("button(4)");
+                        button4 = false;
+                    }
+
+                }
+            }
+        }
+        private void simulationPictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            if ((e.X > 520) && (e.X < 595))
+            {
+                if ((e.Y > 50) && (e.Y < 128))
+                {
+                    // This is in the button 1 area
+                    Console.WriteLine("Mouseclick region 1");
+                    button1 = true;
+                }
+                if ((e.Y > 140) && (e.Y < 220))
+                {
+                    // This is in the button 2 area
+                    Console.WriteLine("Mouseclick region 2");
+                    button2 = true;
+                }
+
+                if ((e.Y > 230) && (e.Y < 309))
+                {
+                    // This is in the button 3 area
+                    Console.WriteLine("Mouseclick region 3");
+                    button3 = true;
+                }
+                if ((e.Y > 320) && (e.Y < 406))
+                {
+                    // This is in the button 4 area
+                    Console.WriteLine("Mouseclick region 4");
+                    button4 = true;
+                }
+
+            }
+        }
+    }
+}
